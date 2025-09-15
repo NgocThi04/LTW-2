0cbd2d49084dde12995f065712916cbe3b74cb72
NgocThi04 <ttnthic720@gmail.com>
2025-09-15 15:25:31 +0700

    Completed P1-P2

---
+++ WebAPI-simple/Data/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI_simple.Models.Domain;
+
+namespace WebAPI_simple.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // define relationship giữa các table bằng Fluent API
+            modelBuilder.Entity<Book_Author>()
+                .HasOne(b => b.Book)
+                .WithMany(ba => ba.Book_Authors)
+                .HasForeignKey(bi => bi.BookId);
+
+            modelBuilder.Entity<Book_Author>()
+                .HasOne(b => b.Author)
+                .WithMany(ba => ba.Book_Authors)
+                .HasForeignKey(bi => bi.AuthorId);
+        }
+
+        public DbSet<Books> Books { get; set; }
+        public DbSet<Authors> Authors { get; set; }
+        public DbSet<Book_Author> Book_Authors { get; set; }
+        public DbSet<Publishers> Publishers { get; set; }
+
+    }
+}
---
+++ WebAPI-simple/Models/Domain/Publishers.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI_simple.Models.Domain
+{
+    public class Publishers
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FullName { get; set; }
+
+        public List<Books> Books { get; set; }
+
+    }
+}
